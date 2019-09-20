56118a5790668b53b8d5218130186f77971e813b
peaksong <kunlunxie@gmail.com>
2019-09-20 23:48:26 +0800

    fix the element import error

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,13 @@
+// todo https://blog.csdn.net/qq_38402659/article/details/100163936
 module.exports = {
   presets: [
     '@vue/app',
-    ['es2015', { 'modules': false }]
+    ['@babel/preset-env', { 'modules': false }]
   ],
-  'plugins': [['component', [
+  'plugins': [['component',
     {
       'libraryName': 'element-ui',
       'styleLibraryName': 'theme-chalk'
     }
-  ]]]
+  ]]
 }
