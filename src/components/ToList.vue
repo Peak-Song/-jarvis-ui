cb68472a0029d5f6b505f3f74382fffb6b03f793
xikunlun <408979832@qq.com>
2019-09-21 19:02:21 +0800

    add normalize.css and sass; prepare to use vue admin ui; delete useless component; add some css

--- src/components/ToList.vue
+++ src/components/ToList.vue
@@ -1,5 +1,16 @@
 <template>
-
+  <div>
+    <el-container>
+      <el-container>
+        <el-main>重要-紧急</el-main>
+        <el-main>重要-不紧急</el-main>
+      </el-container>
+      <el-container>
+        <el-main>不重要-紧急</el-main>
+        <el-main>不重要-不紧急</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
